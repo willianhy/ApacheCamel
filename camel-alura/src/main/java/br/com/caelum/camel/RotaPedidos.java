eaf28c95d3a0c4572f24de6b1c307f91b5d4903c
Willian Yassunaka <willianhy@gmail.com>
2017-07-29 13:45:00 -0300

    Exception handler

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -15,8 +16,24 @@ public class RotaPedidos {
 
 			@Override
 			public void configure() throws Exception {
+				
+				errorHandler(deadLetterChannel("file:error").
+						logExhaustedMessageHistory(true).
+						maximumRedeliveries(3).
+						redeliveryDelay(2000).
+						onRedelivery(new Processor() {
+							
+							@Override
+							public void process(Exchange exchange) throws Exception {
+								int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+								int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+								System.out.println("Redelivery" + counter + "/" + max);
+							}
+						}));
+				
 				from("file:pedidos?delay=5s&noop=true").
 				routeId("rota-pedidos").
+				to("validator:pedido.xsd").
 				multicast().
 					to("direct:soap").
 					to("direct:http");
@@ -30,9 +47,7 @@ public class RotaPedidos {
 					filter().
 						xpath("/item/formato[text()='EBOOK']").
 					setProperty("ebookId", xpath("/item/livro/codigo/text()")).	
-				//	log("${id}").
 					marshal().xmljson().
-				//	log("${body}").
 					setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
 					setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
 				to("http4://localhost:8080/webservices/ebook/item");
